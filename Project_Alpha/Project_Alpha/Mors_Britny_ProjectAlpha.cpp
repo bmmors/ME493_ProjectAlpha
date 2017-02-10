b80b4f1144f50d9f14d73d8011994d8b29e97eff
bmmors <bmmors@gmail.com>
2017-02-09 19:58:14 -0800

    Project Alpha
    Beginning

---
+++ Project_Alpha/Project_Alpha/Mors_Britny_ProjectAlpha.cpp
@@ -0,0 +1,16 @@
+//Britny Mors
+//ME 493 - Autonomy
+//Project Alpha
+//February 16th, 2017
+
+#include <iostream>
+#include <assert.h>
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+	return 0;
+}

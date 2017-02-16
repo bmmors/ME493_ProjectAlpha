991dfbc7754d1314cbf9827167fb34e280b60664
bmmors <bmmors@gmail.com>
2017-02-15 21:51:50 -0800

    Version 7
    Broke

--- Mors_Britny_ProjectAlpha.cpp
+++ Mors_Britny_ProjectAlpha.cpp
@@ -13,10 +13,9 @@
 #include <cmath>
 #include <random>
 #include <time.h>
-#include <fstream>
 
 #define LYRAND (double)rand()/RAND_MAX
-#define PI (double)3.1415
+
 using namespace std;
 
 
@@ -37,6 +36,7 @@ public:
 void MAB::init(){
 	mew = (LYRAND-0.5) * 50;
 	sigma = LYRAND * 25;
+<<<<<<< HEAD
 	alpha = 0.1;
 	value.push_back(0);
 }
@@ -49,25 +49,40 @@ void MAB::pull() {
 		int r_size = reward.size();
 		int v_size = value.size();
 		value.push_back((reward[r_size-1]*alpha) + (value[v_size - 1] * (1 - alpha)));
+=======
+	reward = 0;
+>>>>>>> parent of 926f258... Version 4
 }
 
 int main() {
 	srand(time(NULL));
 
-	int num_arms = 0; //initalize num_arms 
+	int num_arms = 0;
 	cout << "Welcome to Las Vegas! Lets play some slots!\n";
+<<<<<<< HEAD
 	cout << "How many arms does your bandit have?\n" << "Please enter an integer between 1 and 5000:" << endl;
 	cin >> num_arms; //save user input to num arms
+=======
+	cout << "How many arms does your bandit have?\n" << "Please enter an integer:" << endl;
+	cin >> num_arms;
+>>>>>>> parent of 926f258... Version 4
 
-	vector<MAB> bandit_army; //create vector to store bandit information
-	MAB bandito; //create bandit
+	vector<MAB> bandit_army;
+	MAB bandito;
 	
 	for (int i = 0; i < num_arms; i++) {
+<<<<<<< HEAD
 		bandito.init(); //initialize bandit
 		bandit_army.push_back(bandito);//save bandit to vector element
 	}
 	//int x = rand() % (num_arms-1);
 	//bandit_army[x].pull();
+=======
+		bandito.init();
+		bandit_army.push_back(bandito);
+		
+	}
+>>>>>>> parent of 926f258... Version 4
 	
 	return 0;
 }
